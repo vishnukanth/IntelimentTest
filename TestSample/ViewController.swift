99e98c2c64fb7916f47c6a3f287e2a64265dd69d
vishnu <vishnu@cumulations.com>
2016-06-12 11:59:43 +0530

    Initial Commit

---
+++ TestSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSample
+//
+//  Created by vishnukanth on 12/06/16.
+//  Copyright © 2016 Inteliment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
